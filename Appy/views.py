4b0228163cafc4f16d13f912513d9978276c7f93
iamdamified <119352851+iamdamified@users.noreply.github.com>
2025-01-19 21:38:36 +0100

    initializing_values

--- Appy/views.py
+++ Appy/views.py
@@ -54,37 +54,75 @@ def regformpage(request):
 
 
 
-# Format 1 Model Form
-# def formpage(request):
-#     products = Products.objects.all()
-#     if request.method == "POST":
-#         form = ProductsForm(request.POST, request.FILES)#collect data
-#         if form.is_valid(): #validate data
-#             form.save() # save data
-#             return HttpResponse("Your product was successfully created")# then output data
-#     else:
-#         form = ProductsForm()
-
-#     context = {
-#         'product_list': products,
-#         'myform': form
-#     }
-#     return render(request, "Appy/productform.html", context)
-
-# Format 2 Model Form
+# Format 1 Model Form and with Initialization of values.
 def formpage(request):
-    if request.method == "GET":
-        products = Products.objects.all()
-        form = ProductsForm()
-        context = {
-            'product_list': products,
-            'myform': form
-        }
-        
-    elif request.method == "POST":
+    products = Products.objects.all()
+    if request.method == "POST":
         form = ProductsForm(request.POST, request.FILES)#collect data
         if form.is_valid(): #validate data
-            form.save() # save data    
-            return HttpResponse("Your product was successfully created")
-        
+            form.save() # save data
+            return HttpResponse("Your product was successfully created")# then output data
+    else:
+        form = ProductsForm(initial={"name":"Your Name", "description":"short explanation", "price":"0.00","category":"select", "image":"upload"})
+
+    context = {
+        'product_list': products,
+        'myform': form
+    }
     return render(request, "Appy/productform.html", context)
+
+# Format 2 Model Form and without initializing values.
+# def formpage(request):
+#     if request.method == "GET":
+#         products = Products.objects.all()
+#         form = ProductsForm()
+#         context = {
+#             'product_list': products,
+#             'myform': form
+#         }
+        
+#     elif request.method == "POST":
+#         form = ProductsForm(request.POST, request.FILES)#collect data
+#         if form.is_valid(): #validate data
+#             form.save() # save data    
+#             return HttpResponse("Your product was successfully created")
+        
+#     return render(request, "Appy/productform.html", context)
+
+
+
+# Multiple data objects outputs on same page, including a form altogether.
+# def home_page(request):
+
+#     if request.method == "GET":
+#         all_categories = Category.objects.all()
+#         # mens_products = Product.objects.filter(category=2) # A foreignKey called in model for category uses id and not the English name given to the choices as done in Category class
+#         mens_products = Product.objects.filter(category__name="MEN") # This helps the database to recognize and use the Instance name and not its default ID
+#         womens_products = Product.objects.filter(category__name="WOMEN")
+#         kids_products = Product.objects.filter(category__name="kids")
+#         social_images = Social.objects.all()
+
+#         context = {
+#             'all_categories': all_categories,
+#             'mens_product': mens_products,
+#             'womens_product': womens_products,
+#             'kids_product': kids_products,
+#             'social_images': social_images
+
+#         }
+
+#     # Bound instance, this is used to receive entries from a frontend built form into the backend.
+#     elif request.method == "POST":
+#         #  NOTE The name and email we have in the bracket in quotes is pointing to the name and email in quote in the form
+#         new_subscribers_name = request.POST["name"] # OR request.POST.get("name")
+#         new_subscribers_email = request.POST("email") # OR request.POST.get["email"]
+#         # The below is the ORM that describes what operation the application does with the data received from the user throught the form.
+#         Subscriber.objects.create(name=new_subscribers_name, email=new_subscribers_email)
+#         # The below describes what the form reaction would be after submiting
+#         # return HttpResponse("You have subscribed sucessfully")
+#         return redirect("productslink")
+    
+#     return render(request, "myapp/index.html", context)
+
+
+# # Learn more about SENDGRID on twilo.com to know how to distribute newsletters to emails collected from this form (twilo.com/blog/email-activation-django-sendgrid)
